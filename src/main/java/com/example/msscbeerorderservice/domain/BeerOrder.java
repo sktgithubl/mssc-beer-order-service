48b52ca504da28df04ba2ffa3636b93cd47fbf4c
sktgithubl <skt.sipun123@gmail.com>
2021-09-17 12:44:55 +0530

    domain classes added

---
+++ src/main/java/com/example/msscbeerorderservice/domain/BeerOrder.java
@@ -0,0 +1,45 @@
+package com.example.msscbeerorderservice.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BeerOrder extends BaseEntity{
+
+    public BeerOrder(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+                     String customerRef, Customer customer, Set<BeerOrderLine> beerOrderLines,
+                     BeerOrderStatusEnum orderStatus, String orderStatusCallbackUrl) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.customerRef = customerRef;
+        this.customer = customer;
+        this.beerOrderLines = beerOrderLines;
+        this.orderStatus = orderStatus;
+        this.orderStatusCallbackUrl = orderStatusCallbackUrl;
+    }
+
+    private String customerRef;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToMany(mappedBy = "beerOrder", cascade = CascadeType.ALL)
+    @Fetch(FetchMode.JOIN)
+    private Set<BeerOrderLine> beerOrderLines;
+
+    private BeerOrderStatusEnum orderStatus = BeerOrderStatusEnum.NEW;
+    private String orderStatusCallbackUrl;
+}
---
+++ src/main/java/com/example/msscbeerorderservice/domain/Customer.java
@@ -0,0 +1,39 @@
+package com.example.msscbeerorderservice.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Customer extends BaseEntity{
+
+    @Builder
+    public Customer(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+                    String customerName, UUID apiKey, Set<BeerOrder> beerOrders) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.customerName = customerName;
+        this.apiKey = apiKey;
+        this.beerOrders = beerOrders;
+    }
+
+    private String customerName;
+
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)")
+    private UUID apiKey;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
+}
